10d116135a705c6111cf4c1c4f0b5177bec35ea9
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2021-11-12 23:57:53 -0300

    add PyInquirer to interactive command line

---
+++ README.md
@@ -0,0 +1,9 @@
+# Jude PDF Downloader
+
+### Instruções de Uso:
+
+```
+pip install -r requirements.txt
+
+python main.py
+```
--- main.py
+++ main.py
@@ -1,59 +1,111 @@
+import os
+import pathlib
 import json
 import requests
+from PyInquirer import prompt
 
-url = "http://200.128.51.30"
+from examples import custom_style_3
 
-s = requests.Session()
 
-json_data = s.get(url + "/contest-list").content
-contests = json.loads(json_data)["contests"]
+JUDE_URL = 'http://200.128.51.30'
 
-contests.sort(key = lambda x : x['start_time'])
-contests.sort(key = lambda x : x['end_time'], reverse=True)
 
-for index, contest in enumerate(contests):
-	print(f"[{index:2}] {contest['name']}")
-	if index > 15:
-		break
-
-print("=" * 80, end="\n\n")
-
-n = int(input("Contest: "))
-print()
-
-print("=" * 80, end="\n\n")
-print("Auth - Contest:", contests[n]['name'])
-
-login  = input("Login: ")
-passwd = input("Password: ")
-
-data = {
-	"handle": login,
-	"password": passwd,
-	"contest": contests[n]['_id']
+questions = {
+	'contest': {
+		'type': 'list',
+		'name': 'contest',
+		'message': 'Selecione o Contest:',
+		'choices': []
+	},
+	'auth_login': {
+		'type': 'input',
+		'name': 'login',
+		'message': 'Login: '
+	},
+	'auth_passwd': {
+		'type': 'password',
+		'name': 'password',
+		'message': 'Password: '
+	},
+	'problems': {
+		'type': 'checkbox',
+		'name': 'problems',
+		'message': 'Problemas a serem baixados: ',
+		'choices': []
+	},
+	'path': {
+		'type': 'input',
+		'name': 'path',
+		'message': 'Diretório para Download dos PDFs: ',
+	}
 }
 
-s.post(url + "/api-login", data=data)
-
-problems = contests[n]['problems']
-
-for i in range(len(problems)-1):
-	print(f"[{i:2}] {problems[i]['letter']}")
-
-print("=" * 80, end="\n\n")
-
-inicio = int(input("Intervalo inicial: "))
-fim    = int(input("Intervalo   final: "))
-path   = input("Escolha o caminho a ser baixado: ")
-print()
-
-# DOWNLOAD PDF
 
 
-for i in range(inicio, fim+1):
-	pdf = s.get(url + "/contest/statement/" + problems[i]['letter'])
+def main():
 
-	with open(f"{path}/{problems[i]['letter']}.pdf", 'wb') as f:
-		f.write(pdf.content)
+	session = requests.Session()
 
-f.close()
+	json_data = session.get(JUDE_URL + "/contest-list").content
+	list_contests = json.loads(json_data)["contests"]
+
+	list_contests.sort(key = lambda x : x['start_time'])
+	list_contests.sort(key = lambda x : x['end_time'], reverse=True)
+
+	dict_contests = {}
+
+	for contest in list_contests:
+		name = contest['name']
+		dict_aux = { name: contest }
+		dict_contests.update(dict_aux)
+
+	for name, contest in dict_contests.items():
+		questions['contest']['choices'].append(contest['name'])
+
+	main_name = prompt(questions['contest'], style=custom_style_3)['contest']
+
+	main_contest = dict_contests[main_name]
+
+	login  = prompt(questions['auth_login'], style=custom_style_3)['login']
+	passwd = prompt(questions['auth_passwd'], style=custom_style_3)['password']
+
+	data = {
+		"handle": login,
+		"password": passwd,
+		"contest": main_contest['_id']
+	}
+
+	session.post(JUDE_URL + "/api-login", data=data)
+
+	problems = main_contest['problems']
+
+	for problem in problems:
+		letter = problem['letter']
+		dict_aux = { 'name': letter}
+		questions['problems']['choices'].append(dict_aux)
+
+	download_problems = prompt(questions['problems'], style=custom_style_3)
+
+	# PATH CHOICE
+
+	path = prompt(questions['path'], style=custom_style_3)['path']
+
+	# DOWNLOAD PDF
+
+	for problem in download_problems['problems']:
+		pdf = session.get(JUDE_URL + "/contest/statement/" + problem)
+
+		try:
+			with open(f"{path}/{problem}.pdf", 'wb') as f:
+				f.write(pdf.content)
+			print(f"Problema {problem} baixado com sucesso ✔️")
+		
+		except IOError:
+			print("error ao baixar")
+
+
+	f.close()
+
+
+if __name__ == '__main__':
+    main()
